bddcd5a4ab68bceea0fd38d92a66cc7439656906
Hendrik Rahardja <hendrik@alterra.id>
2021-03-28 22:41:18 +0700

    add project and task, show project task on project show list page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,6 +10,7 @@ class ProjectsController < ApplicationController
 
   # GET /projects/1 or /projects/1.json
   def show
+    @task = @project.tasks.build
   end
 
   # GET /projects/new
@@ -63,7 +64,8 @@ class ProjectsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
-      @project = Project.find(params[:id])
+      # @project = Project.find(params[:id])
+      @project = current_user.projects.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
